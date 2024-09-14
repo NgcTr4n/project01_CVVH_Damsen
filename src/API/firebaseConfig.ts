3a4c4d5d0d21bb05e8f653de65fce8d259d7b1af
NgcTr4n <134767754+NgcTr4n@users.noreply.github.com>
2024-09-14 21:48:00 +0700

    Update firebaseConfig.ts

--- src/API/firebaseConfig.ts
+++ src/API/firebaseConfig.ts
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCnvKmgt0pPd3LUSViuYBm0zlumXVqsmJw",
-  authDomain: "tttn-2431.firebaseapp.com",
-  projectId: "tttn-2431",
-  storageBucket: "tttn-2431.appspot.com",
-  messagingSenderId: "59136640485",
-  appId: "1:59136640485:web:9c2caa3454415644e0db0c",
-  measurementId: "G-SNLG1TFREJ"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
